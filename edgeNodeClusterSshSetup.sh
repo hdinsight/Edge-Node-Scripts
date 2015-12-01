c3369e685c65c461d8d72763207291d86d70117b
Tony Murphy <anthony.murphy@microsoft.com>
2015-11-30 18:30:33 -0800

    creates ssh key and puts public key in storage for cluster

---
+++ edgeNodeClusterSshSetup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+vmName=$1
+clusterStorageAccount=$2
+clusterStorageAccountKey=$3
+clusterStorageContainer=$4
+
+privateKeyName="$vmName-cluster-key"
+privateKeyPath="$HOME/.ssh/$privateKeyName"
+
+if [ -f $privateKeyPath ]
+then
+	echo "ssh key exists at $privateKeyPath"
+else
+	ssh-keygen -t rsa -f $privateKeyPath -N '' 
+fi
+publicKeyName="$privateKeyName.pub"
+publicKeyPath="$privateKeyPath.pub"
+
+
+
+sudo apt-get -y -qq install python-pip
+sudo pip install azure-storage
+
+python -c "
+from azure.storage.blob import BlobService
+blob_service=BlobService('$clusterStorageAccount', '$clusterStorageAccountKey')
+blob_service.create_container('$clusterStorageContainer')
+blob_service.put_block_blob_from_path('$clusterStorageContainer', '$publicKeyName','$publicKeyPath', max_connections=5)
+"
