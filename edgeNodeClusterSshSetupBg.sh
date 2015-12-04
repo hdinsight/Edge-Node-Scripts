9b978e1eef028245e1f1fdcb6f9500e7289e529a
Tony Murphy <anthony.murphy@microsoft.com>
2015-12-04 10:56:29 -0800

    background script to finish setup of edge node

---
+++ edgeNodeClusterSshSetupBg.sh
@@ -0,0 +1,17 @@
+sshUserName=$1
+clusterName=$2
+privateKeyName=$3
+privateKeyPath=$4
+
+clusterSshHostName="$clusterName-ssh.azurehdinsight.net"
+echo "Adding cluster host to known hosts if not exist"
+knownHostKey=$(ssh-keygen -H -F $clusterSshHostName 2>/dev/null)
+if [ -z "$knownHostKey" ]
+then
+ssh-keyscan -H $sshUserName >> ~/.ssh/known_hosts
+fi
+
+sudo apt-get install autossh
+autossh -f -i $privateKeyPath $clusterSshHostName
+
+scp -i $privateKeyPath $privateKeyPath $clusterSshHostName:~/.ssh/$privateKeyName
